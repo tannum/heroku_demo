3f55f8c7d245c68bf2a178d29bd0426fcbc9ac0a
tannum <tannummail@gmail.com>
2020-09-08 21:46:24 +0700

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 8080
+
+
+app.get("/", (req, res)=>{
+    res.json({result: "ok", data:[1,2,3,4,5]})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Serer is running. ${PORT}`)
+})
